a83acb849fb5dc8b9609ae6d464408c859054d42
balbarak <balbarak@hotmail.com>
2023-01-15 00:11:12 +0300

    ok  !

--- src/GitflowApp/Program.cs
+++ src/GitflowApp/Program.cs
@@ -3,11 +3,6 @@ var app = builder.Build();
 
 app.MapGet("/", () =>
 {
-    //fix bug ! twooo
-    //another bug !
-    //bug twoo !
-    //Oky
-    //well done bro !
     return "Bug has been fixed !";
 });
 
