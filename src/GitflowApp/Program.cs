2ebbc14294b61ae074462eb2524fd5e290571a43
balbarak <balbarak@hotmail.com>
2023-01-14 23:22:02 +0300

    main !

--- src/GitflowApp/Program.cs
+++ src/GitflowApp/Program.cs
@@ -1,6 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () =>
+{
+
+    return "Ss";
+});
 
 app.Run();
