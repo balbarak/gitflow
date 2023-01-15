7b67052c96b33df588a7754fba2af6d15b596238
balbarak <balbarak@live.com>
2023-01-15 12:59:27 +0300

    update !

--- src/GitflowApp/Program.cs
+++ src/GitflowApp/Program.cs
@@ -6,15 +6,6 @@ var app = builder.Build();
 app.MapGet("/", () =>
 {
     return "Bug has been fixed !";
-    //wesdfasdaf
-    //SADSADF
-    //asdfasdf
-    //sadf
-    //sdfs
-    //sdfsadf
-    //ok
-
-    //ok !
 });
 
 app.Run();
